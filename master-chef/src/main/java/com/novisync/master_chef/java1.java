ade1b49aad7a559d770d9fe033e098d97e3dff5f
Amith <amith@cadrac.org>
2018-09-12 11:27:21 +0530

    Intial Commit

---
+++ master-chef/src/main/java/com/novisync/master_chef/java1.java
@@ -0,0 +1,12 @@
+package com.novisync.master_chef;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class java1 {
+    public static void s(Context c, String message){
+
+        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+
+    }
+}
